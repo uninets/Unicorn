9fd4eaf539cc0ee11044b39f795630b68b4e8521
Mugen Kenichi <mugen.kenichi@uninets.eu>
2012-03-16 18:35:51 +0100

    added git and cpan flags

--- files/streamline.pl
+++ files/streamline.pl
@@ -12,14 +12,70 @@ use Perl::Tidy;
 use File::Find;
 use File::Copy;
 use File::Slurp 'edit_file';
+use Getopt::Long;
 
-sub say_pretty {
+my $git         = 0;
+my $cpan        = 0;
+my $new_version = 0;
+my $debug       = 0;
+
+my $result = GetOptions(
+    'git'   => \$git,
+    'cpan'  => \$cpan,
+    'debug' => \$debug,
+);
+
+$git = 1 if $cpan;
+
+sub say_ok {
     my $line = shift;
+    chomp $line;
 
-    print color 'green';
-    print ' > ';
-    print color 'reset';
+    if ( not $debug ) {
+        print color 'green';
+        print '  > ';
+        print color 'reset';
+    }
     print "$line\n";
+
+    return 1;
+}
+
+sub say_warn {
+    my $line = shift;
+    chomp $line;
+
+    if ( not $debug ) {
+        print color 'yellow';
+        print ' >> ';
+        print color 'reset';
+    }
+    print "$line\n";
+
+    return 1;
+}
+
+sub say_err {
+    my $line = shift;
+    chomp $line;
+
+    if ( not $debug ) {
+        print color 'red';
+        print ' >> ';
+        print color 'reset';
+    }
+    print "$line\n";
+
+    return 1;
+}
+
+sub say_prompt {
+
+    if ( not $debug ) {
+        print color 'blue';
+        print '>>> ';
+        print color 'reset';
+    }
 
     return 1;
 }
@@ -38,11 +94,15 @@ sub bump_version {
             edit_file {s/$old_version/$version/g} $_;
         }
 
-        say_pretty "bumped version from $old_version to $version";
+        $new_version = 1;
+        say_ok "bumped version from $old_version to $version";
 
     }
+    elsif ($cpan) {
+        say_err "No version update. Unable to upload to CPAN";
+    }
     else {
-        say_pretty "no version update";
+        say_warn "No version update.";
     }
 
     return 1;
@@ -51,10 +111,11 @@ sub bump_version {
 sub build_clean {
     if ( -f 'Build' && -x 'Build' ) {
         my $result = qx[./Build clean];
-        say_pretty 'cleaned up';
+        say_warn 'cleaned up';
+        say $result if $debug;
     }
     else {
-        say_pretty 'no need to clean';
+        say_ok 'no need to clean';
     }
 
     return 1;
@@ -65,8 +126,9 @@ sub git_add_new_files {
 
     for (@status) {
         if (/.+new file:\s*(.*)/) {
-            say_pretty "adding $1";
+            say_warn "adding $1";
             my $result = qx[git add $1];
+            say $result if $debug;
         }
     }
 
@@ -74,15 +136,39 @@ sub git_add_new_files {
 }
 
 sub git_commit {
-    say_pretty 'commiting to git repo';
-    my $result = system 'git commit -a 2>&1';
+    say_ok 'git status:';
+    say_warn $_ for qx[git status];
+    say_err 'enter commit message [finish with "."]';
+    say_prompt;
+
+    my $message = '';
+    while (<>) {
+        last if /^.\n/;
+        $message .= $_;
+    }
+
+    say $message if $debug;
+
+    if ($message) {
+        say_ok 'commiting to git repo';
+        my $result = qx[git commit -a -m '$message'];
+
+        say $result if $debug;
+    }
+    else {
+        say_err 'Canceled due to missing commit message.';
+
+        exit 1;
+    }
 
     return 1;
 }
 
 sub git_push {
-    say_pretty 'pushing to remote';
+    say_ok 'pushing to remote';
     my $result = qx[git push 2>&1];
+
+    say $result if $debug;
 
     return 1;
 }
@@ -98,7 +184,7 @@ sub tidy_up {
     );
 
     for (@files) {
-        say_pretty "Tidy up $_";
+        say_ok "Tidy up $_";
         my $destination = "$_.tidy_up";
         Perl::Tidy::perltidy(
             source      => $_,
@@ -115,9 +201,15 @@ build_clean();
 tidy_up();
 bump_version();
 
-git_add_new_files();
-git_commit();
-git_push();
+if ($git) {
+    git_add_new_files();
+    git_commit();
+    git_push();
+}
+
+if ( $cpan && $new_version ) {
+    say_err 'cpan upload not implemented';
+}
 
 exit 0;
 
